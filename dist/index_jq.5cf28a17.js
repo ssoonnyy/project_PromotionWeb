63c95e71f9b1ca39b2a439c10ce642f54a2a91a3
one <sonhw3838@naver.com>
2023-02-14 12:26:25 +0900

    contents02 fade 효과 추가

--- dist/index_jq.5cf28a17.js
+++ dist/index_jq.5cf28a17.js
@@ -118,11 +118,43 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"jQuery/index_jq.js":[function(require,module,exports) {
-$(document).ready(function () {
+$(function () {
   /* 모바일 햄버거 메뉴 버튼 이벤트 */
   $('.m_ham_menu').click(function () {
     $(this).toggleClass('active');
     $('#lnb').toggleClass('openLnb');
+  });
+  $(window).scroll(function () {
+    // header scroll 시 컬러/ stiky 이벤트
+    var target = 0;
+    var st = $(this).scrollTop();
+    if (st > target) {
+      $('.header').addClass('scrolling');
+    } else {
+      $('.header').removeClass('scrolling');
+    }
+    console.log('st', st);
+
+    // 반응형 content02  fade in out 이벤트
+    if (window.innerWidth >= 1920) {
+      if ($('html').scrollTop() >= 800) {
+        $('.con02_left > div').fadeIn(2000);
+      } else {
+        $('.con02_left > div').fadeOut();
+      }
+    } else if (window.innerWidth >= 1024 && window.innerWidth < 1440) {
+      if ($('html').scrollTop() >= 600) {
+        $('.con02_left > div').fadeIn(2000);
+      } else {
+        $('.con02_left > div').fadeOut();
+      }
+    } else if (window.innerWidth >= 801 && window.innerWidth < 1023) {
+      if ($('html').scrollTop() >= 300) {
+        $('.con02_left > div').fadeIn(2000);
+      } else {
+        $('.con02_left > div').fadeOut();
+      }
+    }
   });
 });
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -150,7 +182,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64024" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61109" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- jQuery/index_jq.js
+++ jQuery/index_jq.js
@@ -1,11 +1,48 @@
-$(document).ready(function(){
-
-
+$(function(){
   /* 모바일 햄버거 메뉴 버튼 이벤트 */
   $('.m_ham_menu').click(function(){
     $(this).toggleClass('active');
     $('#lnb').toggleClass('openLnb')
   });
 
+
+
+  $(window).scroll(function(){
+
+    // header scroll 시 컬러/ stiky 이벤트
+    let target = 0;
+    let st = $(this).scrollTop();
+    if(st > target){
+      $('.header').addClass('scrolling');
+    }else {
+      $('.header').removeClass('scrolling');
+    }
+    console.log('st', st)
+  
+
+   
+
+    // 반응형 content02  fade in out 이벤트
+    if(window.innerWidth >= 1920){
+      if($('html').scrollTop() >= 800){
+        $('.con02_left > div').fadeIn(2000);
+      }else{  
+        $('.con02_left > div').fadeOut();
+      }
+    } else if(window.innerWidth >= 1024 && window.innerWidth < 1440){
+      if($('html').scrollTop() >= 600){
+        $('.con02_left > div').fadeIn(2000);
+      }else{  
+        $('.con02_left > div').fadeOut();
+      }
+    }else if(window.innerWidth >= 801 && window.innerWidth < 1023){
+      if($('html').scrollTop() >= 300){
+        $('.con02_left > div').fadeIn(2000);
+      }else{  
+        $('.con02_left > div').fadeOut();
+      }
+    }
+  });
 });
+
 
